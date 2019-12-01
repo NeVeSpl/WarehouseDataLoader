e801d5188de2f4c3928ed5885f213116b5b92b11
NeVeSpl <nevespl@gmail.com>
2019-12-01 13:21:39 +0100

    add StockPartValidatorRegexBased

---
+++ WarehouseDataLoader.Benchmark/StockPartValidatorBenchmark.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using WarehouseDataLoader.Parser.IndexBased.StockPartValidator;
+
+namespace WarehouseDataLoader.Benchmark
+{
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MemoryDiagnoser]
+    public class StockPartValidatorBenchmark
+    {
+        private readonly string[] SampleLines = new[]
+            {
+                "Tokyo,1213|Yokohama,952|Osaka,459",             
+                "Tokyo,1|Yokohama,2|Osaka,3|Nagoya,4|Sapporo,5|Fukuoka,6|Kobe,7|Kawasaki,8|Kyoto,9",
+                "Shelf;1234567890|Shelf,7",
+            };
+
+        [Benchmark]
+        public bool RegexBased()
+        {
+            bool result = false;
+            var validator = new StockPartValidatorRegexBased();           
+            foreach (var line in SampleLines)
+            {
+                result = validator.Validate(line, 0);
+            }
+            return result;
+        }
+
+        [Benchmark]
+        public bool OnStateMachine()
+        {
+            bool result = false;
+            var validator = new StockPartValidatorOnStateMachine();
+            foreach (var line in SampleLines)
+            {
+                result = validator.Validate(line, 0);
+            }
+            return result;
+        }
+    }
+}
---
+++ WarehouseDataLoader/Parser/IndexBased/StockPartValidator/StockPartValidatorRegexBased.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WarehouseDataLoader.Parser.IndexBased.StockPartValidator
+{
+    internal sealed class StockPartValidatorRegexBased : IStockPartValidator
+    {
+        private static readonly Regex pattern = new Regex(@"[^,\|]+,\d{1,9}(?:\|[^,\|]+,\d{1,9})*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+
+        public bool Validate(string line, int stockPartFirstIndex)
+        {
+            Match match = pattern.Match(line, stockPartFirstIndex);
+            if ((match.Success) && (match.Groups[0].Length == line.Length - stockPartFirstIndex))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
